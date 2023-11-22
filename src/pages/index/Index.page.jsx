3d0fc84d91bf5ac637ddd26af0efc87574d9a633
louis <dong.luo@dmall.com>
2023-11-23 00:15:52 +0800

    feat(example): 添加测试demo

--- src/pages/index/Index.page.jsx
+++ src/pages/index/Index.page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import styles from './Index.module.less'
-
+import { Example } from 'remote:glide_components/Example'
 export default function Index() {
-  return <div className={styles.index}>Home Page</div>
+  return <Example className={styles.index}>Home Page2</Example>
 }
